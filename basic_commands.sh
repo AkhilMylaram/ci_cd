c47e7cdeaf14510638aabb87f505fa9d0c87e2e3
Ubuntu <ubuntu@ip-172-31-22-174.ec2.internal>
2024-04-15 12:26:11 +0000

    changed basic_commands .sh

--- basic_commands.sh
+++ basic_commands.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 whoami
-ls
+echo "ls listing files &(ls)"
 echo "current path is $(pwd)"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "system space"
 df -h
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "system memory"
+echo "-------------------------------------------------------"
 free -m
